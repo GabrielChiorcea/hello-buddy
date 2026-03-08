4ce20b40fae49d511704c06a3206fcf2c63b1f91
GabrielChiorcea <gabrielchiorcea93@gmail.com>
2026-03-08 22:53:58 +0200

    orange

--- src/config/themes/index.ts
+++ src/config/themes/index.ts
@@ -19,7 +19,7 @@ export type ThemeName = 'orange' | 'tomato' | 'freshGreen' | 'mustard';
 // ╔═══════════════════════════════════════════╗
 // ║  👇 SCHIMBĂ TEMA AICI (change theme here) ║
 // ╚═══════════════════════════════════════════╝
-export const DEFAULT_THEME: ThemeName = 'mustard';
+export const DEFAULT_THEME: ThemeName = 'orange';
 
 export const themes: Record<ThemeName, ThemePreset> = {
   orange,
